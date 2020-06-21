0484fb54e1c396615be91c9908d4138cb695df4e
Vipul Sharma <vipul.sharma20@gmail.com>
2020-06-21 11:08:44 +0530

    fix: indents

--- lua/jira.lua
+++ lua/jira.lua
@@ -72,89 +72,89 @@ local buf, win
 local position = 0
 
 local function center(str)
-  local width = api.nvim_win_get_width(0)
-  local shift = math.floor(width / 2) - math.floor(string.len(str) / 2)
-  return string.rep(' ', shift) .. str
+   local width = api.nvim_win_get_width(0)
+   local shift = math.floor(width / 2) - math.floor(string.len(str) / 2)
+   return string.rep(' ', shift) .. str
 end
 
 local function open_window()
-  buf = vim.api.nvim_create_buf(false, true)
-  local border_buf = vim.api.nvim_create_buf(false, true)
+    buf = vim.api.nvim_create_buf(false, true)
+    local border_buf = vim.api.nvim_create_buf(false, true)
 
-  vim.api.nvim_buf_set_option(buf, 'bufhidden', 'wipe')
-  vim.api.nvim_buf_set_option(buf, 'filetype', 'jira')
+    vim.api.nvim_buf_set_option(buf, 'bufhidden', 'wipe')
+    vim.api.nvim_buf_set_option(buf, 'filetype', 'jira')
 
-  local width = vim.api.nvim_get_option("columns")
-  local height = vim.api.nvim_get_option("lines")
+    local width = vim.api.nvim_get_option("columns")
+    local height = vim.api.nvim_get_option("lines")
 
-  local win_height = math.ceil(height * 0.8 - 4)
-  local win_width = math.ceil(width * 0.8)
-  local row = math.ceil((height - win_height) / 2 - 1)
-  local col = math.ceil((width - win_width) / 2)
+    local win_height = math.ceil(height * 0.8 - 4)
+    local win_width = math.ceil(width * 0.8)
+    local row = math.ceil((height - win_height) / 2 - 1)
+    local col = math.ceil((width - win_width) / 2)
 
-  local border_opts = {
-    style = "minimal",
-    relative = "editor",
-    width = win_width + 2,
-    height = win_height + 2,
-    row = row - 1,
-    col = col - 1
-  }
+    local border_opts = {
+      style = "minimal",
+      relative = "editor",
+      width = win_width + 2,
+      height = win_height + 2,
+      row = row - 1,
+      col = col - 1
+    }
 
-  local opts = {
-    style = "minimal",
-    relative = "editor",
-    width = win_width,
-    height = win_height,
-    row = row,
-    col = col
-  }
+    local opts = {
+      style = "minimal",
+      relative = "editor",
+      width = win_width,
+      height = win_height,
+      row = row,
+      col = col
+    }
 
-  local border_lines = { '╔' .. string.rep('═', win_width) .. '╗' }
-  local middle_line = '║' .. string.rep(' ', win_width) .. '║'
-  for i=1, win_height do
-    table.insert(border_lines, middle_line)
-  end
-  table.insert(border_lines, '╚' .. string.rep('═', win_width) .. '╝')
-  vim.api.nvim_buf_set_lines(border_buf, 0, -1, false, border_lines)
+    local border_lines = { '╔' .. string.rep('═', win_width) .. '╗' }
+    local middle_line = '║' .. string.rep(' ', win_width) .. '║'
+    for i=1, win_height do
+        table.insert(border_lines, middle_line)
+    end
+    table.insert(border_lines, '╚' .. string.rep('═', win_width) .. '╝')
+    vim.api.nvim_buf_set_lines(border_buf, 0, -1, false, border_lines)
 
-  local border_win = vim.api.nvim_open_win(border_buf, true, border_opts)
-  win = api.nvim_open_win(buf, true, opts)
-  api.nvim_command('au BufWipeout <buffer> exe "silent bwipeout! "'..border_buf)
+    local border_win = vim.api.nvim_open_win(border_buf, true, border_opts)
+    win = api.nvim_open_win(buf, true, opts)
+    api.nvim_command('au BufWipeout <buffer> exe "silent bwipeout! "'..border_buf)
 
-  vim.api.nvim_win_set_option(win, 'cursorline', true)
+    vim.api.nvim_win_set_option(win, 'cursorline', true)
 
-  api.nvim_buf_set_lines(buf, 0, -1, false, { center('My Jira'), '', ''})
-  api.nvim_command('set nofoldenable')
-  api.nvim_buf_add_highlight(buf, -1, 'WhidHeader', 0, 0, -1)
+    api.nvim_buf_set_lines(buf, 0, -1, false, { center('My Jira'), '', ''})
+    api.nvim_command('set nofoldenable')
+    api.nvim_buf_add_highlight(buf, -1, 'WhidHeader', 0, 0, -1)
 end
 
 local function update_view(direction)
-  vim.api.nvim_buf_set_option(buf, 'modifiable', true)
-  position = position + direction
-  if position < 0 then position = 0 end
+    vim.api.nvim_buf_set_option(buf, 'modifiable', true)
+    position = position + direction
+    if position < 0 then position = 0 end
 
-  if results == nil then
-    connect()
-    results = jira:getMyIssues("PCK")
-  end
+    if results == nil then
+        connect()
+        results = jira:getMyIssues("PCK")
+    end
 
-  local result = {}
-  for idx, issue in ipairs(results) do
-    result[idx] = "  " .. issue.key .. ' | ' .. issue.fields.summary
-  end
+    local result = {}
+    for idx, issue in ipairs(results) do
+        result[idx] = "  " .. issue.key .. ' | ' .. issue.fields.summary
+    end
 
-  -- api.nvim_buf_set_lines(buf, 1, 2, false, {center('HEAD~'..position)})
-  api.nvim_buf_set_lines(buf, 3, -1, false, result)
+    -- api.nvim_buf_set_lines(buf, 1, 2, false, {center('HEAD~'..position)})
+    api.nvim_buf_set_lines(buf, 3, -1, false, result)
 
-  api.nvim_buf_add_highlight(buf, -1, 'whidSubHeader', 1, 0, -1)
-  vim.api.nvim_buf_set_option(buf, 'modifiable', false)
+    api.nvim_buf_add_highlight(buf, -1, 'whidSubHeader', 1, 0, -1)
+    vim.api.nvim_buf_set_option(buf, 'modifiable', false)
 end
 
 local function close_window()
-  api.nvim_win_close(win, true)
-  buf = nil
-  win = nil
+    api.nvim_win_close(win, true)
+    buf = nil
+    win = nil
 end
 
 local function split (inputstr, sep)
@@ -169,32 +169,32 @@ local function split (inputstr, sep)
 end
 
 local function move_cursor()
-  local new_pos = math.max(4, api.nvim_win_get_cursor(win)[1] - 1)
-  api.nvim_win_set_cursor(win, {new_pos, 0})
+   local new_pos = math.max(4, api.nvim_win_get_cursor(win)[1] - 1)
+   api.nvim_win_set_cursor(win, {new_pos, 0})
 end
 
 local function set_mappings()
-  local mappings = {
-    ['['] = 'update_view(-1)',
-    [']'] = 'update_view(1)',
-    ['<cr>'] = 'open_file()',
-    q = 'close_window()',
-    k = 'move_cursor()'
-  }
+    local mappings = {
+      ['['] = 'update_view(-1)',
+      [']'] = 'update_view(1)',
+      ['<cr>'] = 'open_file()',
+      q = 'close_window()',
+      k = 'move_cursor()'
+    }
 
-  for k,v in pairs(mappings) do
-    api.nvim_buf_set_keymap(buf, 'n', k, ':lua require"jira".'..v..'<cr>', {
-        nowait = true, noremap = true, silent = true
-      })
-  end
-  local other_chars = {
-    'a', 'c', 'g', 'i', 'n', 'o', 'p', 'r', 's', 't', 'v', 'x', 'y', 'z'
-  }
-  for k,v in ipairs(other_chars) do
-    api.nvim_buf_set_keymap(buf, 'n', v, '', { nowait = true, noremap = true, silent = true })
-    api.nvim_buf_set_keymap(buf, 'n', v:upper(), '', { nowait = true, noremap = true, silent = true })
-    api.nvim_buf_set_keymap(buf, 'n',  '<c-'..v..'>', '', { nowait = true, noremap = true, silent = true })
-  end
+    for k,v in pairs(mappings) do
+        api.nvim_buf_set_keymap(buf, 'n', k, ':lua require"jira".'..v..'<cr>', {
+            nowait = true, noremap = true, silent = true
+          })
+    end
+    local other_chars = {
+      'a', 'c', 'g', 'i', 'n', 'o', 'p', 'r', 's', 't', 'v', 'x', 'y', 'z'
+    }
+    for k,v in ipairs(other_chars) do
+        api.nvim_buf_set_keymap(buf, 'n', v, '', { nowait = true, noremap = true, silent = true })
+        api.nvim_buf_set_keymap(buf, 'n', v:upper(), '', { nowait = true, noremap = true, silent = true })
+        api.nvim_buf_set_keymap(buf, 'n',  '<c-'..v..'>', '', { nowait = true, noremap = true, silent = true })
+    end
 end
 
 local function init()
@@ -204,16 +204,16 @@ local function init()
 end
 
 local function open_file()
-  local s = api.nvim_get_current_line()
+    local s = api.nvim_get_current_line()
 
-  splits = split(s, '|')
-  close_window()
-  init()
+    splits = split(s, '|')
+    close_window()
+    init()
 
-  response = jira:getIssueComments(splits[1]:gsub('%s+', ''))
-  local cjson = require "cjson"
+    response = jira:getIssueComments(splits[1]:gsub('%s+', ''))
+    local cjson = require "cjson"
 
-  api.nvim_buf_set_lines(buf, 0, 10, false, {cjson.encode(response)})
+    api.nvim_buf_set_lines(buf, 0, 10, false, {cjson.encode(response)})
 end
 
 local function jira()
